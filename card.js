10c5a53a4781764ad89b429bf8320ab92421baa9
Busra Yildirim <yldrmbusr@gmail.com>
2021-09-17 23:29:55 +0300

    updated layout

--- card.js
+++ card.js
@@ -8,60 +8,67 @@ let searchText = '';
 let imageIdStartValue = 304;
 
 const fetchAllPost = () => {
-  fetch('https://jsonplaceholder.typicode.com/posts')
-    .then((response) => response.json())
-    .then((data) => {
-      allPosts = data;
-      showCards();
-    });
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((response) => response.json())
+        .then((data) => {
+            allPosts = data;
+            showCards();
+        });
 };
 
 fetchAllPost();
 
 searchInput.addEventListener('input', () => {
-  searchText = searchInput.value.toLowerCase().trim();
-  showCards(searchText);
+    searchText = searchInput.value.toLowerCase().trim();
+    showCards(searchText);
 });
 
 const showCards = (searchText) => {
-  let showedPosts = [];
-  const itemCount = 10;
-  if (searchText) {
-    showedPosts = allPosts
-      .filter((item) => item.title.includes(searchText))
-      .slice(0, itemCount);
-  } else {
-    showedPosts = allPosts.slice(0, itemCount);
-  }
-  section.innerHTML = ''; // reset section
-  displayedCards(showedPosts);
+    let showedPosts = [];
+    const itemCount = 10;
+    if (searchText) {
+        showedPosts = allPosts
+            .filter((item) => item.title.includes(searchText))
+            .slice(0, itemCount);
+    } else {
+        showedPosts = allPosts.slice(0, itemCount);
+    }
+    section.innerHTML = ''; // reset section
+    if (showedPosts.length > 0) {
+        displayedCards(showedPosts);
+    } else {
+        setTimeout(() => {
+            alert("Data not found...")
+        }, 100);
+
+    }
+
 };
 
 const displayedCards = (arr) => {
-  // create card elements
-  arr.forEach((item) => {
-    const card = document.createElement('div');
-    const imageDiv = document.createElement('div');
-    const image = document.createElement('img');
-    const title = document.createElement('div');
-    const desc = document.createElement('div');
+    // create card elements
+    arr.forEach((item) => {
+        const card = document.createElement('div');
+        const imageDiv = document.createElement('div');
+        const image = document.createElement('img');
+        const title = document.createElement('div');
+        const desc = document.createElement('div');
 
-    //classes
-    card.className = 'card';
-    imageDiv.className = 'image';
-    image.src = `https://picsum.photos/id/${imageIdStartValue}/200/180`;
-    imageIdStartValue++;
-    title.className = 'title';
-    desc.className = 'desc ';
-    title.innerText = item.title;
-    desc.innerText = item.body;
+        //classes
+        card.className = 'card';
+        imageDiv.className = 'image';
+        image.src = `https://picsum.photos/id/${imageIdStartValue}/200/180`;
+        imageIdStartValue++;
+        title.className = 'title';
+        desc.className = 'desc ';
+        title.innerText = item.title;
+        desc.innerText = item.body;
 
-    //append
-    imageDiv.appendChild(image);
-    card.appendChild(imageDiv);
-    card.appendChild(title);
-    card.appendChild(desc);
-    section.appendChild(card);
-  });
+        //append
+        imageDiv.appendChild(image);
+        card.appendChild(imageDiv);
+        card.appendChild(title);
+        card.appendChild(desc);
+        section.appendChild(card);
+    });
 };
-
--- form.js
+++ form.js
@@ -1,22 +1,10 @@
-const modal = document.createElement('div');
-modal.className = 'modal';
-
 const submit = document.querySelector("#submit")
-const section = document.querySelector('.form');
-const userInfo = document.createElement('div');
-const company = document.querySelector("#company")
-const fName = document.querySelector("#fName")
-const lName = document.querySelector("#lName")
-const mail = document.querySelector("#e-mail")
-const phone = document.querySelector("#phone")
-
 
 submit.addEventListener("click", () => {
+    Array.from(document.getElementsByClassName("form-row")).forEach(element => {
+        const input = element.children[1]
 
-    consolo.log("company", company.value);
-    consolo.log("fName", fName.value);
-    consolo.log("lName", lName.value);
-    consolo.log("mail", mail.value);
-    consolo.log("phone", phone.value);
+        console.log(input.value);
+    });
 
 })
