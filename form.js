0c2e988af4f21b40c154395f66a9312df504c848
Busra Yildirim <yldrmbusr@gmail.com>
2021-09-17 20:13:11 +0300

    updated form.js

--- form.js
+++ form.js
@@ -1,4 +1,3 @@
-
 const modal = document.createElement('div');
 modal.className = 'modal';
 
@@ -13,27 +12,11 @@ const phone = document.querySelector("#phone")
 
 
 submit.addEventListener("click", () => {
-    console.log("test")
-   // section.style.visible = "hidden"
-  /*  
-    userInfo.innerHTML += company.value + "\n"
-    userInfo.innerHTML += fName.value  + "\n"
-    userInfo.innerHTML += lName.value  + "\n"
-    userInfo.innerHTML += mail.value  + "\n"
-    userInfo.innerHTML += phone.value  + "\n"
-    
-    modal.append(userInfo)
-*/
 
-    e.preventDefault();
-    section.append(modal)
-    modal.style.display="flex";
-    for(let i=0; i<form.elements.length-1; i++){
-       let p =document.createElement("p");
-       p.innerHTML=`${form.elements[i].parentElement.textContent} `+form.elements[i].value;
-       modal.appendChild(p);
-       form.elements[i].value="";
-    }
-    
-}
-)
+    consolo.log("company", company.value);
+    consolo.log("fName", fName.value);
+    consolo.log("lName", lName.value);
+    consolo.log("mail", mail.value);
+    consolo.log("phone", phone.value);
+
+})
